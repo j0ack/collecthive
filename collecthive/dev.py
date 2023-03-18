0f9bc4470b2338ad29a5173770ceafc67c3aed11
TROUVERIE Joachim <jtrouverie@joakode.fr>
2023-03-18 11:17:02 +0100

    new: init app db
    - Use pymongo to connect to a MongoDB in dev
    - Use mongomock to use in tests

--- collecthive/dev.py
+++ collecthive/dev.py
@@ -1,3 +1,4 @@
 INERTIA_TEMPLATE = "base.html"
 SECRET_KEY = "changeme"
 DEBUG = True
+MONGO_URI = "mongodb://localhost:27017/collecthive"
--- collecthive/test.py
+++ collecthive/test.py
@@ -3,3 +3,4 @@ SECRET_KEY = "test"
 SERVER_NAME = "localhost.local"
 TESTING = True
 DEBUG = True
+MONGO_URI = "mongodb://localhost:27017/collecthive_tests"
